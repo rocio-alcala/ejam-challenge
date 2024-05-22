764397bacd87dd5a1d4d6c59cb59c413b18f12f0
Rocio Alcala <rro.alcala@gmail.com>
2024-05-22 12:46:37 -0300

    configs

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <main></main>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
