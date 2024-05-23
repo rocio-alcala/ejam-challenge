7176c587a9792e007f30dbe09575b6fd832ad1e4
Rocio Alcala <rro.alcala@gmail.com>
2024-05-23 13:06:43 -0300

    feat: guarantee card component

---
+++ src/components/GuaranteeCard.tsx
@@ -0,0 +1,15 @@
+import GuaranteeImage from "../assets/guarantee.png";
+
+export default function GuaranteeCard() {
+  return (
+    <div className="flex items-start gap-6 my-6">
+      <img src={GuaranteeImage} className="h-[48px]" />
+      <p>
+        If you are not completely thrilled with your Clarifion - We have a{" "}
+        <span className="font-semibold">30 day satisfaction guarantee.</span>{" "}
+        Please refer to our return policy at the bottom of the page for more
+        details. Happy Shopping
+      </p>
+    </div>
+  );
+}
