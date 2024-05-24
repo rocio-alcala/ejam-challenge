0a54e11b1cd4c10afabf453d2dba68429f7c8e58
Rocio Alcala <rro.alcala@gmail.com>
2024-05-24 12:08:51 -0300

    fix: strech problem in article

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -8,15 +8,12 @@ import ReviewCard from "./ReviewCard";
 
 export default function Article() {
   return (
-    <article className="flex flex-col items-center lg:flex-row lg:gap-5 justify-between my-10 lg:mb-16">
-      <div className="hidden lg:flex flex-col flex-1 h-full justify-between gap-5">
-        <img
-          src={ArticleImage}
-          className="max-h-[591px] object-cover rounded-md"
-        />
+    <article className="flex flex-col items-stretch lg:flex-row lg:gap-5 h-full justify-between my-10 lg:mb-16">
+      <div className="hidden lg:flex flex-col flex-1 justify-between gap-5">
+        <img src={ArticleImage} className="object-cover rounded-md" />
         <ReviewCard></ReviewCard>
       </div>
-      <div className="flex flex-col flex-1 h-full gap-4 justify-between">
+      <div className="flex flex-col flex-1 gap-4 justify-between">
         <h2 className="text-2xl md:text-[32px] text-center lg:text-start">
           <span className="text-[#2C7EF8]">ONE TIME ONLY</span> Special Price
           For 6 Extra Clarifion For Only
